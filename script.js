a70ec16b025b002626b5316fd5684ea919488f38
John Doe <johndoe@example.com>
2022-11-26 22:18:53 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+console.log("Print odds")
+for(var i=1; i<=20; i++)
+if(i%2==0){
+}else{
+    console.log(i)
+ }
+
+ console.log("Decreasing Multiples of 3")
+
+
+for(var i=100; i>=0; i--)
+if(i%3==0){
+    console.log(i)
+}
+
+console.log("Print the sequence")
+for(var i=4; i>=-3.5; i-=1.5)
+console.log(i)
+
+console.log("Sigma")
+var sum=0;
+for(var i=0; i<=100; i++)
+sum+=i;
+console.log("Shuma eshte" , sum)
+
+console.log("Factorial")
+var product=1;
+for(var i=1 ; i<=12; i++)
+product*=i;
+console.log("Prodhimi eshte:" + product)
